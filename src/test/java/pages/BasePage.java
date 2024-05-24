517b26a42a55d3aa58b5d4c2465eafd3297011ef
Wagih <wageh_essam2010@yahoo/com>
2024-05-24 21:55:11 +0300

    commit

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,8 +1,4 @@
 package pages;
 
-import driver.WebDriverSingleton;
-import org.openqa.selenium.WebDriver;
-
 public class BasePage {
-    protected WebDriver driver = WebDriverSingleton.getWebDriver();
 }
